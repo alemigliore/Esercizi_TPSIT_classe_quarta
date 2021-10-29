29d8dede61afcab45e99013d777b5ab2f9222617
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2021-10-29 12:17:10 +0200

    Add files via upload

---
+++ ES_009_tpsit/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define L1 20
+#define L2 40
+
+void cat(char * s1, char *s2)
+{
+    int n1=strlen(s1);
+    int n2=strlen(s2);
+    for(int k=0; k< n2; k++)
+    {
+        *(s1+n1+k)= *(s2+k);
+    }
+    *(s1+n1+n2)='\0';
+}
+
+int main()
+{
+
+    char * s1= (char *) malloc(L2 * sizeof(char));
+    char * s2= (char *) malloc(L1 * sizeof(char));
+    printf("Stringa 1: ");
+    scanf("%s", s1);
+    printf("Stringa 2: ");
+    scanf("%s", s2);
+    cat(s1, s2);
+    printf("Nuova stringa: %s", s1);
+
+    return 0;
+}
